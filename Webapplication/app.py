778e89a3b6f52aff8e9ed822bd25d05fb978d2df
Md Zahidul Islam <106661845+Snigdho8869@users.noreply.github.com>
2023-05-01 10:47:45 +0600

    Used ensembles method

--- Webapplication/app.py
+++ Webapplication/app.py
@@ -1,30 +1,62 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import numpy as np
 import torch
-from transformers import BartForConditionalGeneration, BartTokenizer
+from transformers import BartForConditionalGeneration, BartTokenizer, T5ForConditionalGeneration, T5Tokenizer, GPT2LMHeadModel,GPT2Tokenizer
+
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
 
-@app.route('/', methods=['GET', 'POST'])
+models = {
+    'bart': BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn'),
+    'gpt': GPT2LMHeadModel.from_pretrained('gpt2'),
+}
+
+tokenizers = {
+    'bart': BartTokenizer.from_pretrained('facebook/bart-large-cnn'),
+    'gpt': GPT2Tokenizer.from_pretrained('gpt2'),
+}
+
+@app.route('/')
 def home():
-    if request.method == 'POST':
-        text = request.form['text']
-        if len(text.split()) > 1024:
-            return render_template('index.html', summary="Input text must be less than 1024 tokens.")
-        else:
-            summary = summarize_text(text)
-            return render_template('index.html', text=text, summary=summary)
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
-def summarize_text(text):
-    input_ids = tokenizer.encode(text, return_tensors='pt', max_length=1024)
-    summary_ids = model.generate(input_ids, num_beams=4, length_penalty=2.0, max_length=512, min_length=0, early_stopping=True)
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return summary
+
+@app.route('/text-summarizer', methods=['POST'])
+def text_summarizer():
+    def summarize_text(text, models, tokenizers):
+        summaries = []
+        for model_name, model in models.items():
+            tokenizer = tokenizers[model_name]
+            length_penalty = 2.0
+            num_beams = 3
+            max_length = 1024
+            min_length = max(round(0.33 * len(text.split())), 10)
+            input_ids = tokenizer.encode(text, return_tensors='pt', max_length=max_length, truncation=True)
+            summary_ids = model.generate(input_ids,
+                                      num_beams=num_beams,
+                                      length_penalty=length_penalty,
+                                      max_length=max_length,
+                                      min_length=min_length,
+                                      early_stopping=True,
+                                      pad_token_id=tokenizer.eos_token_id,
+                                      attention_mask=input_ids.new_ones(input_ids.shape))
+            summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+            summaries.append(summary)
+        return summaries
+    
+    text = request.json['text']
+   
+    summaries = summarize_text(text, models, tokenizers)
+    combined_summary = max(set(summaries), key=summaries.count)
+    
+    response = jsonify({'summarizeText': combined_summary})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    
+    return response
+
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
